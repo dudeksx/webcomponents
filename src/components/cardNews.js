e99db62f6f343ca3e37e059f4ef13043e03c6143
Eduardo Porto <dudu05porto@gmail.com>
2023-09-01 13:38:23 -0300

    Adicionando um segundo WebComponent para testar funcionalidades.

--- src/components/cardNews.js
+++ src/components/cardNews.js
@@ -1,3 +1,5 @@
+// Essa classe é responsável por transformar a classe "CardNews" em um documento que retorna elementos HTML
+// Isso é o que chamamos de shadow DOM, pois é um tipo de documento que age "por baixo" dos panos e encapsula um documento jS dentro de documentos HTML, reduzindo a complexidade do documento HTML principal e permitindo dinamismo.
 class CardNews extends HTMLElement {
   constructor() {
     super();
@@ -6,5 +8,5 @@ class CardNews extends HTMLElement {
     shadow.innerHTML = "<h1> Hello World</h1>";
   }
 }
-
+// Uma característica interessante do Shadow DOM é que ele permite criar tags HTML personalizadas. Abaixo criamos uma tag chamada "card-news" que vai trazer o código dentro do bloco da classe CardNews , nesse caso é uma H1 com um Hello World, que será impresso no documento do site.
 customElements.define("card-news", CardNews);
